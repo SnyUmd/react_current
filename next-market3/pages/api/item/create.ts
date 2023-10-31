31de4b34c527bbe82c9207f6ce2ac38e9c9c0e36
DESKTOP-BL\SNY-Beelink <sny.umd@gmail.com>
2023-10-31 10:37:17 +0900

    Next.jsでつくるフルスタックアプリ　前編（バックエンド開発）773/2145ページ

--- next-market3/pages/api/item/create.ts
+++ next-market3/pages/api/item/create.ts
@@ -1,21 +1,31 @@
 import connectDB from "@/utils/database"
+import { ItemModel } from "@/utils/schemaModels"
 
 
 //************************************************************************** */
-const createItem = (req, res) => {
-    // console.log(`=========================`)
-    outLog(`<><><><><><><><><><><><><><>`)
-    console.log(`DB Access : ${connectDB()}`)
-    outLog(`<><><><><><><><><><><><><><>`)
-    // console.log(`=========================`)
-    console.log(req.body)
-    return res.status(200).json({message: "アイテム作成"})
+const createItem = async(req, res) => {
+    try{
+        // console.log(`=========================`)
+        outLog(`<><><><><><><><><><><><><><>`)
+        // console.log(`DB Access : ${await connectDB().toString()}`)
+        await connectDB()
+        outLog(`<><><><><><><><><><><><><><>`)
+        // console.log(`=========================`)
+        console.log(req.body)
+        // console.log(`\n`)
+        outLog(`<><><><><><><><><><><><><><>`)
+        await ItemModel.create(req.body)
+        return res.status(200).json({message: "アイテム作成成功"})
+    }catch(err){
+        return res.status(400).json({message: "アイテム作成失敗"})
+    }
 }
 
 //************************************************************************** */
 const outLog = (mess: string) =>{
     console.log(mess)
 }
+
 
 export default createItem
 
--- next-market3/utils/database.ts
+++ next-market3/utils/database.ts
@@ -3,21 +3,23 @@
 import mongoose from "mongoose"
 
 var dbName = `appDataBase`
+var dbUser = `root`
 var dbPass = `12345roOt`
 
-var mongodbAddress = `mongodb+srv://root:${dbPass}@cluster0.cqdnb95.mongodb.net/${dbName}?retryWrites=true&w=majority`
+var mongodbAddress = `mongodb+srv://${dbUser}:${dbPass}@cluster0.cqdnb95.mongodb.net/${dbName}?retryWrites=true&w=majority`
+// var mongodbAddress = `mongodb+srv://root:12345roOt@cluster0.cqdnb95.mongodb.net/${dbName}?retryWrites=true&w=majority`
 // var mongodbAddress = `mongodb+srv://root:@cluster0.cqdnb95.mongodb.net/?retryWrites=true&w=majority`
 
-const connectDB = () => {
+const connectDB = async() => {
     try{
         // mongoose.connect("mongodb+srv://root:12345roOt@cluster0.cqdnb95.mongodb.net/appDataBase?retryWrites=true&w=majority")
-        mongoose.connect(mongodbAddress)
+        await mongoose.connect(mongodbAddress)
         console.log(`success: Connected to MongoDB`)
-        return true
+        // return true
     }catch(err){
         console.log(`Failure: Unconnected to MongoDB`)
-        throw new Error()
-        return false
+        // throw new Error()
+        // return false
     }
 }
 export default connectDB
